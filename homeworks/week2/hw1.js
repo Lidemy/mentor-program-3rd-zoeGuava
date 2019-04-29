5984584acdcd94431b9854d7dfeb8355ed7077c3
zoeGuava <zoeGuava@gmail.com>
2019-04-29 21:05:47 +0800

    week2 hw1

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,7 +1,10 @@
 function printStars(n) {
-  let i;
-  for (i = 1; i <= n; i += 1) {
-    console.log('*');
+  if (n >= 1 && n <= 30) {
+    let i;
+    for (i = 1; i <= n; i += 1) {
+      console.log('*');
+      // (n >= 1 && (n <= 30)) ? console.log('*') : undefined;
+    }
   }
 }
 printStars(2);
