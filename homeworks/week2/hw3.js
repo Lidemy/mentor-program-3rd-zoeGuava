09c75b8dff65d7c78ed314f328c1186005da9115
zoeGuava <zoeGuava@gmail.com>
2019-05-01 11:16:42 +0800

    week2 hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  return str;
+  const strArr = str.split('');
+  const strRev = [];
+  for (let i = (strArr.length - 1); i >= 0; i -= 1) {
+    strRev.push(strArr[i]);
+  }
+  return strRev.join('');
+  // console.log(str);
 }
-
-reverse('hello');
+reverse('12');
