a12b679e2272eb8e732b0bdac9b17e434fd7f691
zoeGuava <zoeGuava@gmail.com>
2019-05-01 12:32:23 +0800

    week2 hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,26 @@
-function join(str, concatStr) {
-  return concatStr;
-}
+// function join(str, concatStr) {
+//   return concatStr;
+// }
 
-function repeat(str, times) {
-  return times;
-}
+// function repeat(str, times) {
+//   return times;
+// }
 
-console.log(join('a', '!'));
-console.log(repeat('a', 5));
+// console.log(join('a', '!'));
+// console.log(repeat('a', 5));
+
+
+function join(str, conacatStr) {
+  const result = [];
+  for (let i = 0; i < str.length; i += 1) {
+    if (i < str.length - 1) {
+      result.push(str[i] + conacatStr);
+    } else {
+      result.push(str[i]);
+    }
+  }
+  // result = result.toString()
+  return result.toString().replace(/,/g, '');
+  // console.log(finalResult)
+}
+join(['1st', '2nd', '3rd', '4th', '5th'], '!');
