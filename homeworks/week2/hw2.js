9c7702d4b98edbea4ebf700c9779724df1a21692
zoeGuava <zoeGuava@gmail.com>
2019-05-01 11:04:57 +0800

    week hw2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,4 +1,10 @@
 function capitalize(str) {
+  const strArr = str.split('');
+  const strCode = strArr[0].charCodeAt(0);
+  if (strCode >= 97 && strCode <= 122) {
+    strArr[0] = String.fromCharCode(strCode - 32);
+    return strArr.join('');
+  }
   return str;
 }
 
