85b96de689c09ac0e978e8e715ede4f7200f91b1
zoeGuava <zoeGuava@gmail.com>
2019-05-09 10:41:16 +0800

    week3 hw3

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,17 @@
+// function isPrime(n) {
+//   console.log(n);
+// }
+
+// module.exports = isPrime;
+
 function isPrime(n) {
-  console.log(n);
+  for (let i = 2; i <= n; i += 1) {
+    if (n % i === 0 && (i > 1) && (i < n)) {
+      return false;
+    }
+  }
+  return true;
 }
+isPrime(10);
 
 module.exports = isPrime;
